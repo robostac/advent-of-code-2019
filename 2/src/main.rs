a1fb263eb721f3a6822d6ccbab3e4bfdc44afce9
RoboStac <git@robostac.co.uk>
2019-12-02 08:19:59 +0000

    day2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,20 +1,48 @@
 // cargo-deps: text_io
 #[macro_use]
 extern crate text_io;
-use std::io;
-use std::io::prelude::*;
 
+use std::io;
+
+fn run_comp(vec: &Vec<usize>, a: usize, b: usize) -> usize {
+    let mut values = vec.clone();
+    let mut pc: usize = 0;
+    values[1] = a;
+    values[2] = b;
+    while values[pc] != 99 {
+        let (a, b, tgt) = (
+            values[values[pc + 1]],
+            values[values[pc + 2]],
+            values[pc + 3],
+        );
+        // println!("{} {} {} {}", values[pc], tgt, a, b);
+        match values[pc] {
+            1 => values[tgt] = a + b,
+            2 => values[tgt] = a * b,
+            _ => println!("Invalid Opcode {}", values[pc]),
+        }
+        pc += 4;
+    }
+    values[0]
+}
 fn main() {
     let stdin = io::stdin();
-    let values: Vec<i64> = stdin
-        .lock()
-        .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
+    let mut s = String::new();
+    stdin.read_line(&mut s).unwrap();
+    let mut values: Vec<usize> = s
+        .split(",")
+        .map(|x| {
+            let b: usize;
+            scan!(x.bytes() => "{}", b);
+            b
         })
         .collect();
-    println!("{:?}", values);
+    println!("{:?}", run_comp(&values, 12, 2));
+    for a in 0..99 {
+        for b in 0..99 {
+            if (run_comp(&values, a, b)) == 19690720 {
+                println!("{} {} {}", a, b, a * 100 + b)
+            }
+        }
+    }
 }
