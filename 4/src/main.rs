2d0a56ac3281a9da27fa774d14985a6662977cff
robostac <git@robostac.co.uk>
2019-12-04 19:10:00 +0000

    Day4

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -1,20 +1,57 @@
-// cargo-deps: text_io
 #[macro_use]
 extern crate text_io;
 use std::io;
-use std::io::prelude::*;
+
+fn count(cur_prefix: usize, last_value: usize, rem: usize, valid: &dyn Fn(usize) -> bool) -> usize {
+    if rem == 0 {
+        if valid(cur_prefix) {
+            1
+        } else {
+            0
+        }
+    } else {
+        (last_value..=9)
+            .map(|x| count((cur_prefix * 10) + x, x, rem - 1, valid))
+            .sum()
+    }
+}
+
+fn get_digits(mut x: usize) -> [usize; 10] {
+    let mut counts = [0; 10];
+    while x > 0 {
+        counts[x % 10] += 1;
+        x /= 10;
+    }
+    counts
+}
 
 fn main() {
     let stdin = io::stdin();
-    let values: Vec<i64> = stdin
-        .lock()
-        .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
+    let mut s = String::new();
+    stdin.read_line(&mut s).unwrap();
+    let values: Vec<usize> = s
+        .split("-")
+        .map(|x| {
+            let b: usize;
+            scan!(x.bytes() => "{}", b);
+            b
         })
         .collect();
-    println!("{:?}", values);
+    let part1 = |x| {
+        if x < values[0] || x > values[1] {
+            false
+        } else {
+            get_digits(x).iter().any(|&x| x >= 2)
+        }
+    };
+    println!("{:?}", count(0, 0, 6, &part1));
+
+    let part2 = |x| {
+        if x < values[0] || x > values[1] {
+            false
+        } else {
+            get_digits(x).iter().any(|&x| x == 2)
+        }
+    };
+    println!("{:?}", count(0, 0, 6, &part2));
 }
