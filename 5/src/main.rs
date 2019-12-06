78ff8090142331f846d62a64b943fa277cf6ecfc
robostac <git@robostac.co.uk>
2019-12-06 18:38:22 +0000

    Day 5/6

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,20 +1,129 @@
 // cargo-deps: text_io
 #[macro_use]
 extern crate text_io;
+
+use std::collections::VecDeque;
 use std::io;
-use std::io::prelude::*;
+
+struct IntComp {
+    values: Vec<isize>,
+    pc: usize,
+    output: Vec<isize>,
+    input: VecDeque<isize>,
+}
+
+impl IntComp {
+    fn get_vals(&mut self, mut opcode: usize, count: usize) -> Vec<isize> {
+        let mut ret: Vec<isize> = self.values[self.pc..self.pc + count]
+            .iter()
+            .cloned()
+            .collect();
+        opcode /= 100;
+        for v in ret.iter_mut() {
+            if opcode & 1 == 0 {
+                *v = self.values[*v as usize];
+            }
+            opcode /= 10;
+        }
+        self.pc += count;
+        ret
+    }
+    fn add(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        let tgt = self.values[self.pc] as usize;
+        self.pc += 1;
+        self.values[tgt] = x[1] + x[0];
+    }
+    fn mult(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        let tgt = self.values[self.pc] as usize;
+        self.pc += 1;
+        self.values[tgt] = x[1] * x[0];
+    }
+    fn read(&mut self, cmd: usize) {
+        let tgt = self.values[self.pc] as usize;
+        self.pc += 1;
+        self.values[tgt] = self.input.pop_front().unwrap();
+    }
+    fn write(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 1);
+        self.output.push(x[0]);
+    }
+    fn jump_if_true(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        if x[0] != 0 {
+            self.pc = x[1] as usize;
+        }
+    }
+    fn jump_if_false(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        if x[0] == 0 {
+            self.pc = x[1] as usize;
+        }
+    }
+    fn less_than(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        let tgt = self.values[self.pc] as usize;
+        self.pc += 1;
+        self.values[tgt] = if x[0] < x[1] { 1 } else { 0 };
+    }
+    fn equals(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 2);
+        let tgt = self.values[self.pc] as usize;
+        self.pc += 1;
+        self.values[tgt] = if x[0] == x[1] { 1 } else { 0 };
+    }
+    fn run(&mut self) -> Vec<isize> {
+        loop {
+            let cmd = self.values[self.pc] as usize;
+            let cmd_byte = cmd % 100;
+            self.pc += 1;
+            // println!("OPCODE {} {} {}", cmd_byte, cmd, self.pc);
+            match cmd_byte {
+                1 => self.add(cmd),
+                2 => self.mult(cmd),
+                3 => self.read(cmd),
+                4 => self.write(cmd),
+                5 => self.jump_if_true(cmd),
+                6 => self.jump_if_false(cmd),
+                7 => self.less_than(cmd),
+                8 => self.equals(cmd),
+                _ => {
+                    // println!("EXIT OPCODE {} {}", cmd_byte, cmd);
+                    break;
+                }
+            };
+        }
+        self.output.clone()
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
-    let values: Vec<i64> = stdin
-        .lock()
-        .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
+    let mut s = String::new();
+    stdin.read_line(&mut s).unwrap();
+    let values: Vec<isize> = s
+        .split(",")
+        .map(|x| {
+            let b: isize;
+            scan!(x.bytes() => "{}", b);
+            b
         })
         .collect();
-    println!("{:?}", values);
+    let mut ic = IntComp {
+        values: values.clone(),
+        pc: 0,
+        input: vec![1].into_iter().collect(),
+        output: Vec::new(),
+    };
+    ic.run();
+    println!("{:?}", ic.output);
+    let mut ic = IntComp {
+        values: values.clone(),
+        pc: 0,
+        input: vec![5].into_iter().collect(),
+        output: Vec::new(),
+    };
+    ic.run();
+    println!("{:?}", ic.output);
 }
--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,20 +1,58 @@
-// cargo-deps: text_io
-#[macro_use]
-extern crate text_io;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io;
 use std::io::prelude::*;
+fn count(x: &String, dict: &HashMap<String, Vec<String>>) -> usize {
+    match dict.get(x) {
+        Some(s) => s.iter().map(|x| count(x, &dict) + 1).sum(),
+        None => 0,
+    }
+}
+
+fn bfs(start: String, tgt: String, dict: &HashMap<String, Vec<String>>) -> usize {
+    let mut current: HashSet<&String> = [&start].iter().cloned().collect();
+    let mut inc = 0;
+    let mut seen = HashSet::<&String>::new();
+    while current.is_empty() == false {
+        current = current
+            .iter()
+            .map(|x| dict[*x].iter().collect())
+            .collect::<Vec<Vec<&String>>>()
+            .concat()
+            .iter()
+            .cloned()
+            .collect();
+        current = current.difference(&seen).cloned().collect();
+        seen.extend(current.iter().cloned());
+        if seen.contains(&tgt) {
+            return inc - 1;
+        }
+        inc += 1;
+    }
+    0
+}
 
 fn main() {
     let stdin = io::stdin();
-    let values: Vec<i64> = stdin
-        .lock()
-        .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
-        })
-        .collect();
-    println!("{:?}", values);
+    let mut orbits = HashMap::new();
+    let mut links = HashMap::new();
+    for x in stdin.lock().lines() {
+        let z = x.unwrap();
+        let s: Vec<&str> = z.split(")").collect();
+        orbits
+            .entry(s[0].to_owned())
+            .or_insert(Vec::<String>::new())
+            .push(s[1].to_owned());
+        links
+            .entry(s[1].to_owned())
+            .or_insert(Vec::<String>::new())
+            .push(s[0].to_owned());
+        links
+            .entry(s[0].to_owned())
+            .or_insert(Vec::<String>::new())
+            .push(s[1].to_owned());
+    }
+    let v: usize = orbits.keys().map(|x| count(x, &orbits)).sum();
+    println!("{}", v);
+    println!("{:?}", bfs("YOU".to_owned(), "SAN".to_owned(), &links));
 }
