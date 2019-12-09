d54e6b275cd80fc36f03e0756c55fff39696845f
robostac <git@robostac.co.uk>
2019-12-09 06:29:49 +0000

    Day 9 + Day7 update

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -1,28 +1,41 @@
 // cargo-deps: text_io
 #[macro_use]
 extern crate text_io;
-
+use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::io;
 #[derive(Clone, Debug)]
 struct IntComp {
-    values: Vec<isize>,
+    values: HashMap<usize, isize>,
     pc: usize,
+    rel: isize,
     output: VecDeque<isize>,
     input: VecDeque<isize>,
     finished: bool,
 }
 
 impl IntComp {
-    fn get_vals(&mut self, mut opcode: usize, count: usize) -> Vec<isize> {
-        let mut ret: Vec<isize> = self.values[self.pc..self.pc + count]
-            .iter()
-            .cloned()
+    fn get_vals(&mut self, mut opcode: usize, count: usize) -> Vec<(isize, Option<usize>)> {
+        let mut ret: Vec<(isize, Option<usize>)> = (0..count)
+            .map(|x| (*self.values.entry(self.pc + x).or_insert(0), None))
             .collect();
         opcode /= 100;
         for v in ret.iter_mut() {
-            if opcode & 1 == 0 {
-                *v = self.values[*v as usize];
+            match opcode % 10 {
+                0 => {
+                    *v = (
+                        *self.values.entry(v.0 as usize).or_insert(0),
+                        Some(v.0 as usize),
+                    )
+                }
+                1 => (),
+                2 => {
+                    *v = (
+                        *self.values.entry((self.rel + v.0) as usize).or_insert(0),
+                        Some((self.rel + v.0) as usize),
+                    )
+                }
+                _ => panic!("Unknown Mode {}", opcode % 10),
             }
             opcode /= 10;
         }
@@ -30,74 +43,76 @@ impl IntComp {
         ret
     }
     fn add(&mut self, cmd: usize) {
-        let x = self.get_vals(cmd, 2);
-        let tgt = self.values[self.pc] as usize;
-        self.pc += 1;
-        self.values[tgt] = x[1] + x[0];
+        let x = self.get_vals(cmd, 3);
+        *self.values.entry(x[2].1.unwrap()).or_insert(0) = x[1].0 + x[0].0;
     }
     fn mult(&mut self, cmd: usize) {
-        let x = self.get_vals(cmd, 2);
-        let tgt = self.values[self.pc] as usize;
-        self.pc += 1;
-        self.values[tgt] = x[1] * x[0];
+        let x = self.get_vals(cmd, 3);
+        *self.values.entry(x[2].1.unwrap()).or_insert(0) = x[1].0 * x[0].0;
     }
     fn read(&mut self, cmd: usize) {
-        let tgt = self.values[self.pc] as usize;
-        self.pc += 1;
-        self.values[tgt] = self.input.pop_front().unwrap();
+        if self.input.is_empty() {
+            self.pc -= 1;
+        } else {
+            let x = self.get_vals(cmd, 1);
+            *self.values.entry(x[0].1.unwrap()).or_insert(0) = self.input.pop_front().unwrap();
+        }
     }
     fn write(&mut self, cmd: usize) {
         let x = self.get_vals(cmd, 1);
-        self.output.push_back(x[0]);
+        self.output.push_back(x[0].0);
     }
     fn jump_if_true(&mut self, cmd: usize) {
         let x = self.get_vals(cmd, 2);
-        if x[0] != 0 {
-            self.pc = x[1] as usize;
+        if x[0].0 != 0 {
+            self.pc = x[1].0 as usize;
         }
     }
     fn jump_if_false(&mut self, cmd: usize) {
         let x = self.get_vals(cmd, 2);
-        if x[0] == 0 {
-            self.pc = x[1] as usize;
+        if x[0].0 == 0 {
+            self.pc = x[1].0 as usize;
         }
     }
     fn less_than(&mut self, cmd: usize) {
-        let x = self.get_vals(cmd, 2);
-        let tgt = self.values[self.pc] as usize;
-        self.pc += 1;
-        self.values[tgt] = if x[0] < x[1] { 1 } else { 0 };
+        let x = self.get_vals(cmd, 3);
+        *self.values.entry(x[2].1.unwrap()).or_insert(0) = if x[0].0 < x[1].0 { 1 } else { 0 };
     }
     fn equals(&mut self, cmd: usize) {
-        let x = self.get_vals(cmd, 2);
-        let tgt = self.values[self.pc] as usize;
-        self.pc += 1;
-        self.values[tgt] = if x[0] == x[1] { 1 } else { 0 };
+        let x = self.get_vals(cmd, 3);
+        *self.values.entry(x[2].1.unwrap()).or_insert(0) = if x[0].0 == x[1].0 { 1 } else { 0 };
     }
+    fn rel_update(&mut self, cmd: usize) {
+        let x = self.get_vals(cmd, 1);
+        self.rel += x[0].0
+    }
+
+    fn step(&mut self) {
+        if self.finished {
+            return;
+        }
+        let cmd = self.values[&self.pc] as usize;
+        let cmd_byte = cmd % 100;
+        self.pc += 1;
+        //println!("OPCODE {} {} {} {}", cmd_byte, cmd, self.pc, self.rel);
+        match cmd_byte {
+            1 => self.add(cmd),
+            2 => self.mult(cmd),
+            3 => self.read(cmd),
+            4 => self.write(cmd),
+            5 => self.jump_if_true(cmd),
+            6 => self.jump_if_false(cmd),
+            7 => self.less_than(cmd),
+            8 => self.equals(cmd),
+            9 => self.rel_update(cmd),
+            99 => self.finished = true,
+            _ => panic!("Unknown Opcode {}", cmd_byte),
+        };
+    }
+
     fn run(&mut self) {
-        loop {
-            let cmd = self.values[self.pc] as usize;
-            let cmd_byte = cmd % 100;
-            self.pc += 1;
-            // println!("OPCODE {} {} {}", cmd_byte, cmd, self.pc);
-            match cmd_byte {
-                1 => self.add(cmd),
-                2 => self.mult(cmd),
-                3 => self.read(cmd),
-                4 => {
-                    self.write(cmd);
-                    break;
-                }
-                5 => self.jump_if_true(cmd),
-                6 => self.jump_if_false(cmd),
-                7 => self.less_than(cmd),
-                8 => self.equals(cmd),
-                _ => {
-                    self.finished = true;
-                    // println!("EXIT OPCODE {} {}", cmd_byte, cmd);
-                    break;
-                }
-            };
+        while self.finished == false {
+            self.step();
         }
     }
 }
@@ -133,17 +148,16 @@ fn permute2(comp: &IntComp, used: usize, mut input: [isize; 5], cur: usize) -> i
         for i in 0..5 {
             comps[i].input.push_back(input[i]);
         }
-        let mut val = 0;
+        comps[0].input.push_back(0);
         while comps[4].finished == false {
             for x in 0..5 {
-                comps[x].input.push_back(val);
-                comps[x].run();
-                if comps[x].finished == false {
-                    val = comps[x].output.pop_front().unwrap();
-                }
+                comps[x].step();
+                let copy = &comps[x].output.clone();
+                comps[(x + 1) % 5].input.extend(copy);
+                comps[x].output.clear();
             }
         }
-        return val;
+        return comps[0].input.pop_front().unwrap();
     }
     (5..10)
         .filter_map(|x| {
@@ -171,8 +185,9 @@ fn main() {
         })
         .collect();
     let ic = IntComp {
-        values: values.clone(),
+        values: values.iter().cloned().enumerate().collect(),
         pc: 0,
+        rel: 0,
         input: VecDeque::new(),
         output: VecDeque::new(),
         finished: false,
