4ed7891a018ca5ae7e55cc21b566145203b341b4
robostac <res@robostac.co.uk>
2019-12-24 13:52:41 +0000

    Day 21 + 22

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,20 +1,63 @@
 // cargo-deps: text_io
-#[macro_use]
-extern crate text_io;
-use std::io;
-use std::io::prelude::*;
+use intcomp::IntComp;
+use std::collections::HashMap;
+use std::collections::HashSet;
+const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (0, 1), (-1, 0), (1, 0)];
 
+fn run(inst: &[&str], ic: &IntComp) -> Option<isize> {
+    let mut tilesic = ic.clone();
+    let mut grid = HashMap::new();
+    for p in inst.iter() {
+        for x in p.bytes() {
+            tilesic.push_input(x as isize);
+        }
+        tilesic.push_input('\n' as isize);
+    }
+    tilesic.run();
+    let mut x = 0;
+    let mut y = 0;
+    for v in tilesic.output_ref() {
+        if *v > 1000 {
+            draw(&grid);
+            return Some(*v);
+        }
+        if *v == 10 {
+            y += 1;
+            x = 0;
+        } else {
+            grid.insert((x, y), *v);
+            x += 1;
+        }
+    }
+    draw(&grid);
+    None
+}
 fn main() {
-    let stdin = io::stdin();
-    let values: Vec<i64> = stdin
-        .lock()
-        .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
-        })
-        .collect();
-    println!("{:?}", values);
+    let ic = IntComp::build_from_stdin();
+    let inst = [
+        "NOT D J", "NOT J J", "OR A T", "AND A T", "AND B T", "AND C T", "NOT T T", "AND T J",
+        "WALK",
+    ];
+    println!("{:?}", run(&inst, &ic));
+    let inst = [
+        "NOT E T", "NOT T T", "OR H T", "AND D T", "NOT T J", "NOT J J", "OR A T", "AND A T",
+        "AND B T", "AND C T", "NOT T T", "AND T J", "RUN",
+    ];
+    println!("{:?}", run(&inst, &ic));
+}
+
+fn draw(gamestate: &HashMap<(isize, isize), isize>) {
+    let minx = gamestate.keys().map(|x| x.0).min().unwrap();
+    let miny = gamestate.keys().map(|x| x.1).min().unwrap();
+    let maxx = gamestate.keys().map(|x| x.0).max().unwrap();
+    let maxy = gamestate.keys().map(|x| x.1).max().unwrap();
+    println!("{} {} {} {}", minx, maxx, miny, maxy);
+    for y in miny..=maxy {
+        for x in minx..=maxx {
+            let v = *gamestate.get(&(x, y)).or(Some(&32)).unwrap();
+
+            print!("{}", v as u8 as char);
+        }
+        println!();
+    }
 }
--- 22/src/main.rs
+++ 22/src/main.rs
@@ -1,20 +1,92 @@
-// cargo-deps: text_io
-#[macro_use]
-extern crate text_io;
+use modinverse::modinverse;
 use std::io;
 use std::io::prelude::*;
 
+fn shuffle(ncards: i64, mut pos: i64, inst: &Vec<Vec<String>>) -> i64 {
+    for x in inst.iter() {
+        if x[0] == "cut" {
+            let v = x[1].parse::<i64>().unwrap();
+            pos = (pos - v) % ncards;
+        } else if x[0] == "deal" && x[1] == "with" {
+            let v = x[3].parse::<i64>().unwrap();
+            pos = (pos * v) % ncards
+        } else if x[0] == "deal" && x[1] == "into" {
+            pos = ncards - 1 - pos;
+        }
+    }
+    pos
+}
+
+fn rev_shuffle(ncards: i64, mut pos: i64, inst: &Vec<Vec<String>>) -> i64 {
+    for x in inst.iter().rev() {
+        if x[0] == "cut" {
+            let v = x[1].parse::<i64>().unwrap();
+            pos = (pos + v) % ncards;
+        } else if x[0] == "deal" && x[1] == "with" {
+            let v = x[3].parse::<i64>().unwrap();
+            let mut z = pos;
+            while z % v != 0 {
+                z += ncards;
+            }
+            pos = (z / v) % ncards;
+        } else if x[0] == "deal" && x[1] == "into" {
+            pos = ncards - 1 - pos;
+        }
+    }
+    pos
+}
+
+fn mod_pow(mut base: i128, mut exp: i128, modulus: i128) -> i128 {
+    if modulus == 1 {
+        return 0;
+    }
+    let mut result = 1;
+    base = base % modulus;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % modulus;
+        }
+        exp = exp >> 1;
+        base = base * base % modulus
+    }
+    result
+}
+
+fn apply(x: &Vec<String>, a: i128, b: i128, modval: i128) -> (i128, i128) {
+    if x[0] == "cut" {
+        let v = x[1].parse::<i128>().unwrap();
+        (a, (b - v) % modval)
+    } else if x[1] == "into" {
+        (-a % modval, (modval - 1 - b) % modval)
+    } else {
+        let a128 = a as i128;
+        let b128 = b as i128;
+        let m128 = modval as i128;
+        let v = x[3].parse::<i128>().unwrap();
+        (((a128 * v) % m128) as i128, ((b128 * v) % m128) as i128)
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
-    let values: Vec<i64> = stdin
+    let values: Vec<Vec<String>> = stdin
         .lock()
         .lines()
-        .map(|input| {
-            let x: i64;
-            let s = input.unwrap();
-            scan!(s.bytes() => "{}", x);
-            x
-        })
+        .map(|input| input.unwrap().split(" ").map(|x| x.to_owned()).collect())
         .collect();
-    println!("{:?}", values);
+    println!("{:?}", shuffle(10007, 2019, &values));
+    let m = 119315717514047i128;
+    let cards = 2020i128;
+    let n = 101741582076661i128;
+    let mut a = 1i128;
+    let mut b = 0i128;
+    for x in values.iter() {
+        let c = apply(&x, a, b, m);
+        a = c.0;
+        b = c.1;
+    }
+    let modinv = modinverse(1 - a, m).unwrap();
+    let r = (b * modinv) % m;
+    let pos = (((cards - r) * mod_pow(a, n * (m - 2), m) + r) + m) % m;
+    println!(" {:?}", pos);
 }
